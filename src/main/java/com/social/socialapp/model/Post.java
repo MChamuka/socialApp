3181f93d1958080121f3155ed5f80cf03f441213
unknown <maddewithanagechamuka@gmail.com>
2025-06-04 01:57:57 +0530

    added post model

---
+++ src/main/java/com/social/socialapp/model/Post.java
@@ -0,0 +1,18 @@
+package com.social.socialapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Getters and Setters
+}
